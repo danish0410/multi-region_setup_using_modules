4ce7a83b7722a4dcd779ad8991cc35ceda8a111f
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2026-01-22 18:56:23 +0530

    added docker in  userdata

--- multi-region-ec2/modules/region/userdata/dev_classic_userdata.sh
+++ multi-region-ec2/modules/region/userdata/dev_classic_userdata.sh
@@ -21,6 +21,41 @@ apt-get install -y \
   gnupg \
   lsb-release
 
+# -------------------------
+# DOCKER INSTALL (UPDATED)
+# -------------------------
+install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+apt-get install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
+
+systemctl enable docker
+systemctl start docker
+
+usermod -aG docker ubuntu
+
+docker --version
+docker compose version
+# -------------------------
+# DOCKER INSTALL END
+# -------------------------
+
 # Install Ansible
 add-apt-repository --yes --update ppa:ansible/ansible
 apt-get install -y ansible
@@ -36,5 +71,7 @@ export AWS_REGION="$REGION"
 # Run Ansible
 cd /opt/create-user/ansible
 ansible-playbook create-user.yml
+
+sudo apt install awscli -y
 
 echo "===== USERDATA COMPLETED SUCCESSFULLY ====="
